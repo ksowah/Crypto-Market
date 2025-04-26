99cdf93ff9c4b19825da05c855dbb8c1848758d5
ksowah <sowahkelvin640@gmail.com>
2025-04-26 23:04:01 +0000

    Initial Commit

---
+++ CryptoMonitor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CryptoMonitor
+//
+//  Created by macbook on 26/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoMonitor/CryptoMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMonitorApp.swift
+//  CryptoMonitor
+//
+//  Created by macbook on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
